70193fb558e2cf7ad1eeb89b00ce8d60621f81cb
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2025-11-12 22:01:23 +0300

    микро фикс

--- utils/umk_composer/notes_proccesor.py
+++ utils/umk_composer/notes_proccesor.py
@@ -6,10 +6,10 @@ def create_note_from_metadata(
         settings : dict, 
         data : dict, 
         item_type : str, 
-        get_name, 
-        item_prop = None, 
-        item_choise = None,
-        promt = None):
+        get_item_name, 
+        get_item_prop = None, 
+        get_item_choise = None,
+        get_item_promt = None):
     logger.level("CREATING_NOTES", no=11, color="<green>")
 
     # собираем мета данные определенных типов
@@ -21,35 +21,36 @@ def create_note_from_metadata(
             items.append(item)
 
     # фильтруем записи 
-    if item_choise:
-        items = item_choise(settings, items)
+    if get_item_choise:
+        items = get_item_choise(settings, items)
 
     # перебераем мета данные
     for n, item in enumerate(items):
-        file_name = get_name(n, item)
+        file_name = get_item_name(n, item)
 
         if os.path.exists(file_name):
             logger.log("CREATING_NOTES",f"пропускаем файл {file_name}, он уже создан")
             continue
 
         with open(f"{file_name}", "w+", encoding="UTF-8") as item_file:
-            if item_prop:
-                item_file.write(item_prop(settings, item))
+            if get_item_prop:
+                item_file.write(get_item_prop(settings, item))
                 item_file.write("\n")
 
-            if promt:
+            if get_item_promt:
                 item_file.write("<promt>\n")
-                item_file.write(promt(settings, item))
+                item_file.write(get_item_promt(settings, item))
                 item_file.write("</promt>\n")
         
             logger.log("CREATING_NOTES",f"файл {file_name} создан")
 
 
-def create_lection_notes(base_dir, settings, data):
+def create_lection_notes( settings, data):
     create_note_from_metadata(
         settings,
         data,
         "лекция",
-        get_name = lambda _n, _note: f"{base_dir}/Лекция №{_n+1}",
-        item_prop=lambda _settings, _note: "pisi"
+        get_item_name = lambda _n, _note: f"{settings['teoretical_part_path']}/Лекция №{_n+1}.md",
+        get_item_prop=lambda _settings, _note: "pisi",
+        get_item_promt=lambda _settings, _note: "pisi",
     )
